adf1660f502bc934fe0c14a0b06aba06dac6fd22
ocanath <ocanath@gmail.com>
2022-11-10 20:57:15 -0800

    added some c string parsing stuff and tested working (slow as fuck) udp server

--- udp_server/udp_server.ino
+++ udp_server/udp_server.ino
@@ -1,35 +1,11 @@
 #include "WiFi.h"
 #include "WiFiUdp.h"
-
+#include "Preferences.h"
+#include "parse_console.h"
 
 #define IPV4_ADDR_ANY   0x00000000UL
 
-enum {PERIOD_CONNECTED = 150, PERIOD_DISCONNECTED = 3000};
-
-
-#define CHAR_BUF_SIZE 256
-uint8_t gl_char_buf[256] = {0};
-int gl_charbuf_loc = 0;
-
-void parse_console_input(void)
-{
-    int rv = Serial.read();
-    if(rv != -1)
-    {
-      uint8_t rchar = (uint8_t)rv;
-      if(gl_charbuf_loc < CHAR_BUF_SIZE)
-      {
-        gl_char_buf[gl_charbuf_loc++] = rchar;
-      }
-      else
-      {
-        gl_charbuf_loc = 0;
-      }
-
-      //Serial.write((char)rv);
-      Serial.printf("0x%0.2X",rchar);
-    }
-}
+enum {PERIOD_CONNECTED = 50, PERIOD_DISCONNECTED = 3000};
 
 WiFiUDP udp;
 
@@ -57,30 +33,56 @@ void loop() {
   Serial.begin(460800);
 
   IPAddress server_address((uint32_t)IPV4_ADDR_ANY); //
-  udp.begin(server_address,11054);
+  udp.begin(server_address, 3425);
 
 
   uint32_t blink_ts = 0;
   uint32_t blink_period = PERIOD_DISCONNECTED;
   uint8_t led_mode = 0;
 
-
   uint8_t udp_pkt_buf[256] = {0};
   while(1)
   {
 
-    int len = udp.read(udp_pkt_buf,255);
-    if(len > 0)
+    int len = udp.parsePacket();
+    if(len != 0)
     {
+      int len = udp.read(udp_pkt_buf,255);
       udp_pkt_buf[len] = 0; //clear last element to null term string before writing
       Serial.write("Received: ");
       Serial.write((const char *)udp_pkt_buf);
       Serial.write("\r\n");
       for(int i = 0; i < len; i++)
         udp_pkt_buf[i] = 0;
+
+      udp.beginPacket(udp.remoteIP(), udp.remotePort());
+      udp.printf("received: %s\r\n", udp_pkt_buf);
+      udp.endPacket();
     }
 
-    parse_console_input();
+    get_console_lines();
+
+
+    if(gl_console_cmd.parsed == 0)
+    {
+      if(strcmp((const char *)gl_console_cmd.buf,"ipconfig\r") == 0)
+      {
+        Serial.printf("IP address is: 0x%0.8X\r\n", WiFi.localIP());
+      }
+      else
+      {
+        Serial.printf("Failed to parse: %s\r\n", gl_console_cmd.buf);
+      }
+
+
+
+      for(int i = 0; i < BUFFER_SIZE; i++)
+      {
+        gl_console_cmd.buf[i] = 0; 
+      }
+      gl_console_cmd.parsed = 1;
+    }
+
 
     if(WiFi.status() != WL_CONNECTED)
     {
