a8b27fb5e0ccd061650c2d38b7c3bf26356e39c8
ocanath <ocanath@gmail.com>
2023-08-23 12:06:23 -0700

    update to ip finding mechanism

--- pyClient/udp_ip_finder.py
+++ pyClient/udp_ip_finder.py
@@ -3,7 +3,19 @@ import time
 import struct 
 import numpy as np
 
-udp_server_addr = ("192.168.29.255", 34345)
+hostname = socket.gethostname()
+addr=socket.gethostbyname(hostname)
+print("Host IP Addr: "+addr)
+addrmod = addr
+dotidx = len(addrmod)-1
+for i in range(len(addrmod)-1,0, -1):
+	if(addrmod[i] == '.'):
+		dotidx = i
+		break
+lastoctet = addrmod[dotidx+1:len(addrmod)]
+addrmod = addrmod.replace(lastoctet,"255")
+print("Using: "+addrmod)
+udp_server_addr = (addrmod, 34345)
 bufsize = 512
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(0.0) #make non blocking
@@ -23,8 +35,8 @@ while(time.time()-start_time < 3):
 		if(len(pkt) > 0):
 			print("received: "+str(pkt)+"from: ",str(addr))
 			found = 1
+		break
 	except:
 		pass
-
 if(found == 0):
 	print("no response")
